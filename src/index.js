c014587fca3f2bfbeda44c68631012b05f7527b8
EngelberAmaya <engelberamaya@gmail.com>
2023-05-15 15:03:07 -0400

    CRUD - Leer usuarios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // import { obtenerChiste } from "./js/http-provider";
 
-import { init } from "./js/usuarios-page";
+//import { init } from "./js/usuarios-page";
 
 // obtenerChiste().then(console.log);
 
 
-init();
+//init();
+
+import * as CRUD from './js/crud-provider';
+
+CRUD.getUsuario(1).then(console.log);
